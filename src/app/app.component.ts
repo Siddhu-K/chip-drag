09bcb93c76c21da43e9d4899f73100fa49cc97a6
Siddhu-K <siddharth@krishya.co>
2025-02-20 14:18:52 +0530

    add angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DragDropModule, MatChipsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'chip-drag';
